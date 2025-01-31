2e705bfe582afcc00ad647521269c6ce9d69ef2a
Meghanaputhur <puthurmeghana@gmail.com>
2025-01-31 16:25:10 +0530

    commiting from dev

--- wipro_java_project/src/com/wipro/java/HelloWorld.java
+++ wipro_java_project/src/com/wipro/java/HelloWorld.java
@@ -19,6 +19,8 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		System.out.println("My first java program");
 		// TODO Auto-generated method stub
 
 	}
