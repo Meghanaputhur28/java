e60afee38ad8251ad39213213efd02d92da1b9a4
Meghanaputhur <puthurmeghana@gmail.com>
2025-02-04 16:07:29 +0530

    Day3

---
+++ wipro_java_project/src/com/wipro/java/exception/CheckedException.java
@@ -0,0 +1,22 @@
+package com.wipro.java.exception;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CheckedException {
+	
+	public static void main(String [] args) {
+		try {
+			File file = new File("Wrong Path");
+			FileReader reader = new FileReader(file);
+			
+		}
+		catch(IOException e) {
+			System.out.println(e.getStackTrace());
+			System.out.println("wrong file have been selected kindly  check the path");
+		}
+
+	}
+
+}
