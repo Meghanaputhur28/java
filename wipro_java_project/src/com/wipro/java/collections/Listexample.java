b41044b55c8094db18564c79864befc478e96f3f
Meghanaputhur <puthurmeghana@gmail.com>
2025-02-05 14:36:02 +0530

    Day4

---
+++ wipro_java_project/src/com/wipro/java/collections/Listexample.java
@@ -0,0 +1,36 @@
+package com.wipro.java.collections;
+
+import java.util.List;
+import java.util.*;
+
+public class Listexample {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> l1 = new ArrayList<Integer>();
+		l1.add(0, 1);
+		l1.add(1, 2);
+		l1.add(2, 2);
+		System.out.println(l1);
+		
+		List<Integer> l2  = new ArrayList<Integer>();
+		l2.add(1);
+		l2.add(2);
+		l2.add(3);
+		
+		
+		l1.addAll(1, l2);
+		
+		System.out.println(l1);
+		
+		l1.remove(1);
+		System.out.println(l1);
+		System.out.println(l1.get(2));
+		l1.set(0, 5);
+		System.out.println(l1);
+		
+
+	}
+
+}
