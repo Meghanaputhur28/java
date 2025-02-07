05c758d90ae91ae909c13878a1b024984414ce17
Meghanaputhur <puthurmeghana@gmail.com>
2025-02-07 11:54:32 +0530

    Day5

--- wipro_java_project/src/com/wipro/java/java8/StreamApi.java
+++ wipro_java_project/src/com/wipro/java/java8/StreamApi.java
@@ -18,5 +18,15 @@ import java.util.stream.Collectors;
 			List<String> stream1 = list.stream().
 				map(String::toUpperCase).collect(Collectors.toList());
 				System.out.println(stream1);
+			List<Integer> number = Arrays.asList(6,4,7,8);
+			List<Integer> Number = number.stream()
+				   .sorted()
+				   .collect(Collectors.toList());
+			System.out.println(Number);
+			List<Integer> newnumbers = Arrays.asList(10, 20, 5, 25, 30);// Creating another list of integers
+			int min = newnumbers.stream().min(Integer::compare).get();// Finding the minimum value in the list
+			int max = newnumbers.stream().max(Integer::compare).get();// Finding the maximum value in the list
+			System.out.println("Min: " + min);
+			System.out.println("Max: " + max);
 		}
 }
